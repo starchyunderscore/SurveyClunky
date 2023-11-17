456fd8ca712990ffc6fafd3d469246c4ecaaa632
starchyunderscore <starchy_underscore@gmx.com>
2023-11-16 19:32:09 -0500

    2

--- index.py
+++ index.py
@@ -1,12 +1,16 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import unquote
 import time
 import html
+import uuid
 
 hostName = "localhost"
 serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
+    def form_creator(file):
+        
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -19,7 +23,7 @@ class MyServer(BaseHTTPRequestHandler):
             elif self.path == "/create":
                 self.wfile.write(open("./www/create.html", "rb").read())
             elif self.path.startswith("/take"):
-                self.wfile.write(open(self.path, "rb").read())
+                self.wfile.write(bytes(form_creator(open(self.path, "rt").read())))
             elif self.path.startswith("/results"):
                 print("results")
             else:
@@ -31,7 +35,17 @@ class MyServer(BaseHTTPRequestHandler):
         print("post")
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length)
-        print(post_data.decode('utf-8'))
+        form_data = post_data.decode("utf-8").replace("+", " ")
+        form_data = unquote(form_data)
+        form_data = form_data[4:]
+        form_data = html.escape(form_data)
+        
+        uuid = uuid.uuid4()
+        
+        form_file = open("myfile.txt", "w")
+        
+        print(form_data)
+        
         
         self._set_response()
         self.wfile.write(bytes("<html><head><title>Created</title></head><body>", "utf-8"))
